0f9cce3e30f97fa7198a5d0a55d2afef75177055
rightear01 <rightear01@gmail.com>
2024-03-22 17:43:40 +0900

    update

---
+++ change.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageEnhance
+
+# Load the image
+img_path = 'C:/brighter_123.jpg'
+image = Image.open(img_path)
+
+# Enhance the brightness
+enhancer = ImageEnhance.Brightness(image)
+brighter_image = enhancer.enhance(0.6)  # Increase brightness by 10%
+
+# Save the enhanced image
+output_path = 'brighter_123.jpg'
+brighter_image.save(output_path)
+output_path
